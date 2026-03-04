3957b955a279fe851ef63fef12ab59203f3e8989
polino <polinovskyoleksandr1303@gmail.com>
2026-03-04 11:40:48 +0100

    made the setup, the flight model, the basic structure and paths

--- luggages/models.py
+++ luggages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Flight(models.Model):
+    flight_number = models.CharField(max_length=6)
+    departure_date = models.DateField
+    return_date = models.DateField
+
+    def __str__(self):
+        return self.flight_number
--- luggages/views.py
+++ luggages/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Flight
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>This is the homepage</h1>')
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def flight_index(request):
+    flights = Flight.objects.all()
+    return render(request, 'flights/index.html', {'flights':flights})
